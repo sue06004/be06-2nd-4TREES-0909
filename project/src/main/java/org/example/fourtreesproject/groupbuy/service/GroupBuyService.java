1f18d261a8a7dcf24b223622834f701b207a3bac
kangkigs <dlflq11@gmail.com>
2024-07-17 14:14:18 +0900

    [Feat] 등록된 입찰 조회 구현중 (#31)

--- project/src/main/java/org/example/fourtreesproject/groupbuy/service/GroupBuyService.java
+++ project/src/main/java/org/example/fourtreesproject/groupbuy/service/GroupBuyService.java
@@ -2,16 +2,21 @@ package org.example.fourtreesproject.groupbuy.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.fourtreesproject.bid.model.entity.Bid;
+import org.example.fourtreesproject.bid.repository.BidRepository;
 import org.example.fourtreesproject.groupbuy.model.entity.Category;
 import org.example.fourtreesproject.groupbuy.model.entity.GroupBuy;
 import org.example.fourtreesproject.groupbuy.model.request.GroupBuyCreateRequest;
+import org.example.fourtreesproject.groupbuy.model.response.RegisteredBidListResponse;
 import org.example.fourtreesproject.groupbuy.repository.CategoryRepository;
 import org.example.fourtreesproject.groupbuy.repository.GroupBuyRepository;
+import org.example.fourtreesproject.product.repository.ProductRepository;
 import org.example.fourtreesproject.user.model.entity.User;
 import org.example.fourtreesproject.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +25,8 @@ public class GroupBuyService {
     private final GroupBuyRepository gpbuyRepository;
     private final UserRepository userRepository;
     private final CategoryRepository categoryRepository;
+    private final BidRepository bidRepository;
+    private final ProductRepository productRepository;
 
     public boolean save(GroupBuyCreateRequest request) {
         User user = userRepository.findById(request.getUserIdx()).get();
@@ -41,5 +48,9 @@ public class GroupBuyService {
 
         return true;
 
+    }
+
+    public List<RegisteredBidListResponse> findBidList(Long gpbuyIdx) {
+        Bid bid = bidRepository.findByGpbuyIdx(gpbuyIdx);
     }
 }
