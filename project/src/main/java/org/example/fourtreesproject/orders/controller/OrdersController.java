30a28c022c657cbb2f36a149bc3b04a6258d5178
sue06004 <sue06004@naver.com>
2024-07-18 23:41:04 +0900

    [Docs] OrderController swagger 설정 (#99)

--- project/src/main/java/org/example/fourtreesproject/orders/controller/OrdersController.java
+++ project/src/main/java/org/example/fourtreesproject/orders/controller/OrdersController.java
@@ -2,6 +2,7 @@ package org.example.fourtreesproject.orders.controller;
 
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.sun.jna.platform.unix.X11;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.example.fourtreesproject.common.BaseResponse;
 import org.example.fourtreesproject.orders.model.response.OrderPageResponse;
@@ -23,6 +24,7 @@ public class OrdersController {
 
     private final OrdersService ordersService;
 
+    @Operation(summary = "결제(공구 시작, 공구 참여) api")
     @GetMapping("/register")
     public BaseResponse<String> orderRegister(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                               String impUid) throws IamportResponseException, IOException, RuntimeException {
@@ -31,6 +33,7 @@ public class OrdersController {
         return new BaseResponse<>();
     }
 
+    @Operation(summary = "자신의 주문 정보 조회 api")
     @GetMapping("/info/list")
     public BaseResponse<List<OrdersListResponse>> readOrderInfoList(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                                    Integer page, Integer size) throws RuntimeException {
@@ -38,6 +41,7 @@ public class OrdersController {
         return new BaseResponse<>(orderInfoList);
     }
 
+    @Operation(summary = "주문 페이지 정보 조회 api")
     @GetMapping("/page")
     public BaseResponse<OrderPageResponse> getOrderPageInfo(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                             Long orderIdx) throws RuntimeException {
