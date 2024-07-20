3148e14ee560d1b444c9f40eb8cc98d400212fb6
seo-jae-eun <wodms5522@naver.com>
2024-07-20 23:51:36 +0900

    [Fix] 사업자등록번호 인증 api swagger 기본값 설정 (#132)

--- project/src/main/java/org/example/fourtreesproject/companyRegVerify/controller/CompanyRegVerifyController.java
+++ project/src/main/java/org/example/fourtreesproject/companyRegVerify/controller/CompanyRegVerifyController.java
@@ -1,6 +1,8 @@
 package org.example.fourtreesproject.companyRegVerify.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import lombok.RequiredArgsConstructor;
 import org.example.fourtreesproject.common.BaseResponse;
 import org.example.fourtreesproject.companyRegVerify.model.request.CompanyRegVerifyRequest;
@@ -16,7 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompanyRegVerifyController {
     private final CompanyRegVerifyService companyRegVerifyService;
 
-    @Operation(summary = "사업자등록번호 인증 api")
+    @Operation(summary = "사업자등록번호 인증 api",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = {
+                                @ExampleObject(name = "Valid example", value = """
+                               {
+                                 "b_no": "1208800767",
+                                 "p_nm": "강한승",
+                                 "start_dt": "20130716"
+                               }"""),
+                                @ExampleObject(name = "Invalid email", value = """
+                               {
+                                 "b_no": "1208800768",
+                                 "p_nm": "서재은",
+                                 "start_dt": "20130717"
+                               }"""),
+                                @ExampleObject(name = "Default", value = """
+                               {
+                                 "nickname": "string",
+                                 "email": "string",
+                                 "password": "string"
+                               }""")
+                            }
+                    )
+            )
+    )
     @PostMapping("/verify")
     public BaseResponse verify(@RequestBody CompanyRegVerifyRequest companyRegVerifyRequest) {
         return companyRegVerifyService.validate(companyRegVerifyRequest);
