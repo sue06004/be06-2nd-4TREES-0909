f6d01c269fc6c678058f6d73e4186649ef014c02
jimnyy <dojimny1004@gmail.com>
2024-07-23 17:14:58 +0900

    [Fix] 상품 등록 Response 수정

---
+++ project/src/main/java/org/example/fourtreesproject/product/model/response/ProductRegisterResponse.java
@@ -0,0 +1,10 @@
+package org.example.fourtreesproject.product.model.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ProductRegisterResponse {
+    private Long productIdx;
+}
